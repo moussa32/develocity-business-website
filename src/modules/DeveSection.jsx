0361b5e344d4156dd60f1efc533a5809e76f49ad
mozes32 <moussaebrahem32@gmail.com>
2023-01-10 23:12:05 +0200

    feat: added deve section

---
+++ src/modules/DeveSection.jsx
@@ -0,0 +1,38 @@
+const DeveSection = () => {
+  return (
+    <div className="bg-black text-center py-16">
+      <div className="container mx-auto">
+        <p className="text-xs text-indigo-400 capitalize">for successful digital transformation</p>
+        <h3 className="text-white font-bold text-4xl mt-2.5">Why Develocity</h3>
+        <div className="grid px-4 text-left mt-10 gap-6 md:grid-cols-2 lg:grid-cols-3 md:px-0">
+          <div className="bg-secondary p-5 rounded-lg">
+            <h4 className="text-white mt-6">Full service cycle</h4>
+            <p className="text-text mt-2">
+              We can provide a full-service solution for your software development projects, from architecture design to
+              production deployment. Our professional teams handle challenges at any stage of the product development
+              cycle.
+            </p>
+          </div>
+          <div className="bg-secondary p-5 rounded-lg">
+            <h4 className="text-white mt-6">Full service cycle</h4>
+            <p className="text-text mt-2">
+              We can provide a full-service solution for your software development projects, from architecture design to
+              production deployment. Our professional teams handle challenges at any stage of the product development
+              cycle.
+            </p>
+          </div>
+          <div className="bg-secondary p-5 rounded-lg">
+            <h4 className="text-white mt-6">Full service cycle</h4>
+            <p className="text-text mt-2">
+              We can provide a full-service solution for your software development projects, from architecture design to
+              production deployment. Our professional teams handle challenges at any stage of the product development
+              cycle.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DeveSection;
