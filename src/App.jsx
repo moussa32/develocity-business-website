4059370c2b152fe847d3ff6cd510a5a4ba4b30d5
mozes32 <moussaebrahem32@gmail.com>
2023-01-11 01:21:31 +0200

    feat: added metaverse section

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,18 @@ import Cryptocurrency from "./modules/Cryptocurrency";
 import Empowering from "./modules/Empowering";
 import NFTGameDev from "./modules/NFTGameDev";
 import DeveSection from "./modules/DeveSection";
+import MetaverseSection from "./modules/MetaverseSection";
 
 const App = () => {
   return (
     <div className="bg-[rgba(7,7,7)] text-whiteText text-neutral-50">
-      <Empowering />
-      <DeveSection />
-      <Cryptocurrency />
-      <NFTGameDev />
+      <div className="container mx-auto">
+        <Empowering />
+        <DeveSection />
+        <Cryptocurrency />
+        <NFTGameDev />
+        <MetaverseSection />
+      </div>
     </div>
   );
 };
