683d3654284578aec3a58fd7d1846f0240e08d14
mozes32 <moussaebrahem32@gmail.com>
2023-01-11 00:08:06 +0200

    refactor: change name of svg icon

--- src/modules/DeveSection.jsx
+++ src/modules/DeveSection.jsx
@@ -1,4 +1,4 @@
-import NFTGameSvg from "../assets/NFTGame.svg";
+import DoubleArrowIcon from "../assets/images/DoubleArrowIcon.svg";
 
 const DeveSection = () => {
   return (
@@ -8,7 +8,7 @@ const DeveSection = () => {
         <h3 className="text-white text-[48px] mt-1.5 font-PolySans font-normal mb-12">Why Develocity</h3>
         <div className="grid px-4 text-left mt-10 gap-8 md:grid-cols-2 lg:grid-cols-3 md:px-0">
           <div className="bg-neutral-800/50 p-5 rounded-lg">
-            <img src={NFTGameSvg} />
+            <img src={DoubleArrowIcon} alt="double arrow" title="double arrow" />
             <h4 className="text-white mt-8 text-xl font-medium">Full service cycle</h4>
             <p className="text-neutral-400 mt-2.5">
               We can provide a full-service solution for your software development projects, from architecture design to
@@ -17,7 +17,7 @@ const DeveSection = () => {
             </p>
           </div>
           <div className="bg-neutral-800/50 p-5 rounded-lg">
-            <img src={NFTGameSvg} />
+            <img src={DoubleArrowIcon} alt="double arrow" title="double arrow" />
             <h4 className="text-white mt-8 text-xl font-medium">Discovery phase</h4>
             <p className="text-neutral-400 mt-2.5">
               Every project starts with a discovery phase where we gather complete requirements and take a deep dive
@@ -26,7 +26,7 @@ const DeveSection = () => {
             </p>
           </div>
           <div className="bg-neutral-800/50 p-5 rounded-lg">
-            <img src={NFTGameSvg} />
+            <img src={DoubleArrowIcon} alt="double arrow" title="double arrow" />
             <h4 className="text-white mt-8 text-xl font-medium">Quality-driven team</h4>
             <p className="text-neutral-400 mt-2.5">
               We’re obsessed with quality development and strive for perfection. Our project and delivery managers align
