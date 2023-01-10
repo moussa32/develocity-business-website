5644f23dfb5dbc3fa40fc6f4424713af7919005e
MennaAllah Ahmed <78885127+mennallahmed@users.noreply.github.com>
2023-01-10 23:01:50 +0200

    edit empowering section

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import NFTGameDev from "./modules/NFTGameDev";
 
 const App = () => {
   return (
-    <div className="App bg-secondary text-whiteText text-neutral-50">
+    <div className="App bg-[rgba(7,7,7)] text-whiteText text-neutral-50">
       <Empowering />
       <Cryptocurrency />
       <NFTGameDev />
