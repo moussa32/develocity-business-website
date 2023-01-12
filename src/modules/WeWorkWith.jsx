2c509f5ddd279362e0a7ea9520d7d40c3b15419f
Malihassan <momenalhendawy@gmail.com>
2023-01-12 21:03:35 +0200

    fixed text responsive

--- src/modules/WeWorkWith.jsx
+++ src/modules/WeWorkWith.jsx
@@ -20,8 +20,8 @@ function WeWorkWith() {
         <h3 className="text-white font-bold text-4xl my-2.5">
           Blockchains we work with
         </h3>
-        <div className="grid px-4 text-left mt-28 gap-6 md:grid-cols-2 lg:grid-cols-4 md:px-0">
-          <div className="w-11/12 mb-6 relative flex justify-center">
+        <div className="grid px-4 text-left mt-28 gap-6 md:grid-cols-2 lg:grid-cols-4 md:px-0 flex justify-center">
+          <div className="mb-6 relative flex justify-center">
             <div className="z-0 absolute h-16 w-20 -top-7">
               <img className="blur-lg" src={`${BinanceEllipse}`} />
             </div>
@@ -30,7 +30,7 @@ function WeWorkWith() {
               <span className="text-xl">Binance</span>
             </div>
           </div>
-          <div className="w-11/12 mb-6 relative flex justify-center">
+          <div className="mb-6 relative flex justify-center">
             <div className="z-0 absolute h-16 w-20 -top-7">
               <img className="blur-lg" src={`${EthereumEllipse}`} />
             </div>
@@ -39,7 +39,7 @@ function WeWorkWith() {
               <span className="text-xl">Ethereum</span>
             </div>
           </div>
-          <div className="w-11/12 mb-4 relative flex justify-center">
+          <div className="mb-4 relative flex justify-center">
             <div className="z-0 absolute h-16 w-16 -top-7">
               <img src={`${PoloygonEllipse}`} />
             </div>
@@ -65,7 +65,7 @@ function WeWorkWith() {
               </div>
             </div>
           </div>
-          <div className="w-11/12 mb-4 relative flex justify-center">
+          <div className="mb-4 relative flex justify-center">
             <div className="z-0 absolute h-16 w-20 -top-7">
               <img className="blur-lg" src={`${SolanaEllipse}`} />
             </div>
