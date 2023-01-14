d3fca1195e724da131b73484d132212d2911e1fc
mozes32 <moussaebrahem32@gmail.com>
2023-01-14 21:36:58 +0200

    refactor: make menu sticky

--- src/modules/Navbar.jsx
+++ src/modules/Navbar.jsx
@@ -1,22 +1,20 @@
 import React from "react";
 import navLogo from "../assets/NavLogo.svg";
-import SideMenu from "./SideMenu";
 
 const Navbar = () => {
   return (
-    <nav className="container pt-12 mx-auto">
-      <div className="flex flex-col md:flex-row justify-between items-center">
-        <img src={`${navLogo}`} />
+    <nav className="container pt-12 mx-auto relative">
+      <div className="flex flex-col md:flex-row items-center fixed w-full z-40">
+        <img src={`${navLogo}`} alt="logo" className="block" />
         <div
           className="w-[300px] md:w-[403px] hidden mt-3 h-[53px] bg-opacity-5 bg-white text-neutral-300 text-base
-        rounded-[20px] md:flex justify-evenly items-center"
+        rounded-[20px] md:flex md:mt-0 justify-evenly items-center backdrop-blur-md translate-x-2/4 xl:ml-[271px]"
         >
           <a>Home</a>
           <a>Company</a>
           <a>Services</a>
           <a>Contact</a>
         </div>
-        <div></div>
       </div>
     </nav>
   );
