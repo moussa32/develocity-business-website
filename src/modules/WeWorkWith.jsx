6c5e8b16a49b5c8d619218c4eb0d2d91f0441a42
mozes32 <moussaebrahem32@gmail.com>
2023-01-14 22:05:01 +0200

    fix: blockchain section cards styles & added hover

--- src/modules/WeWorkWith.jsx
+++ src/modules/WeWorkWith.jsx
@@ -14,53 +14,65 @@ function WeWorkWith() {
   return (
     <div className="bg-black text-center my-16">
       <div className="container mx-auto">
-        <p className="text-xs text-indigo-400 capitalize">
-          for successful digital transformation
-        </p>
-        <h3 className="text-white font-bold text-4xl my-2.5">
-          Blockchains we work with
-        </h3>
-        <div className="grid px-4 text-left mt-28 gap-6 md:grid-cols-2 lg:grid-cols-4 md:px-0 flex justify-center">
-          <div className="mb-6 relative flex justify-center">
-            <div className="z-0 absolute h-16 w-20 -top-7">
-              <img className="blur-lg" src={`${BinanceEllipse}`} />
+        <p className="text-xs text-indigo-400 capitalize">for successful digital transformation</p>
+        <h3 className="text-white font-bold text-4xl my-2.5">Blockchains we work with</h3>
+        <div className="grid px-4 text-left mt-28 gap-6 md:grid-cols-2 lg:grid-cols-4 md:px-0">
+          <div className="mb-6 relative flex justify-center group">
+            <div className="z-0 absolute h-16 w-16 -top-7">
+              <img src={`${BinanceEllipse}`} />
             </div>
-            <div className="bg-neutral-900  rounded-lg h-80 md:w-10/12 w-full flex flex-col justify-center items-center">
-              <img className="mb-4" src={`${Binance}`} />
-              <span className="text-xl">Binance</span>
-            </div>
-          </div>
-          <div className="mb-6 relative flex justify-center">
-            <div className="z-0 absolute h-16 w-20 -top-7">
-              <img className="blur-lg" src={`${EthereumEllipse}`} />
-            </div>
-            <div className="bg-neutral-900  rounded-lg h-80 md:w-10/12 w-full flex flex-col justify-center items-center">
-              <img className="mb-4" src={`${Ethereum}`} />
-              <span className="text-xl">Ethereum</span>
+            <div className="bg-neutral-700/25 group-hover:bg-gradient-to-b group-hover:from-[#312E81] group-hover:to-indigo-500 backdrop-blur-lg z-10 rounded-lg h-80 md:w-10/12 w-full flex flex-col justify-center">
+              <div className="pt-6 h-full md:h-4/6 flex flex-col justify-between items-center mt-14 pb-8 md:pb-0">
+                <div className="pt- flex flex-col items-center">
+                  <img className="mb-4" src={`${Binance}`} />
+                  <span className="text-xl">Binance</span>
+                </div>
+                <p className="text-sm mx-3 hidden group-hover:flex">
+                  https://Binance.com
+                  <img className="ml-5 pb-1" src={`${Arrow}`} />
+                </p>
+              </div>
             </div>
           </div>
-          <div className="mb-6 relative flex justify-center">
+          <div className="mb-6 relative flex justify-center group">
+            <div className="z-0 absolute h-16 w-16 -top-7">
+              <img src={`${EthereumEllipse}`} />
+            </div>
+            <div className="bg-neutral-700/25 group-hover:bg-gradient-to-b group-hover:from-[#312E81] group-hover:to-indigo-500 backdrop-blur-lg z-10 rounded-lg h-80 md:w-10/12 w-full flex flex-col justify-center">
+              <div className="pt-6 h-full md:h-4/6 flex flex-col justify-between items-center mt-14 pb-8 md:pb-0">
+                <div className="pt- flex flex-col items-center">
+                  <img className="mb-4" src={`${Ethereum}`} />
+                  <span className="text-xl">Ethereum</span>
+                </div>
+                <p className="text-sm mx-3 hidden group-hover:flex">
+                  https://Ethereum.com
+                  <img className="ml-5 pb-1" src={`${Arrow}`} />
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="mb-6 relative flex justify-center group">
             <div className="z-0 absolute h-16 w-16 -top-7">
               <img src={`${PoloygonEllipse}`} />
             </div>
-            <div className="bg-indigo-800 z-10 rounded-lg h-80 md:w-10/12 w-full flex flex-col justify-center">
+            <div className="bg-neutral-700/25 group-hover:bg-gradient-to-b group-hover:from-[#312E81] group-hover:to-indigo-500 backdrop-blur-lg z-10 rounded-lg h-80 md:w-10/12 w-full flex flex-col justify-center">
               <div className="pt-6 h-full md:h-4/6 flex flex-col justify-between items-center mt-14 pb-8 md:pb-0">
                 <div className="pt- flex flex-col items-center">
                   <img className="mb-4" src={`${Polygon}`} />
                   <span className="text-xl">Polygon MATIC</span>
                 </div>
-                <p className="text-sm mx-3 flex">
+                <p className="text-sm mx-3 hidden group-hover:flex">
                   https://polygonscan.com
-                  <img className="ml-5 pb-1" src={`${Arrow}`}/>
+                  <img className="ml-5 pb-1" src={`${Arrow}`} />
                 </p>
               </div>
             </div>
           </div>
-          <div className="mb-4 relative flex justify-center">
-            <div className="z-0 absolute h-16 w-20 -top-7">
-              <img className="blur-lg" src={`${SolanaEllipse}`} />
+          <div className="mb-4 relative flex justify-center group">
+            <div className="z-0 absolute h-16 w-20 -top-8">
+              <img src={`${SolanaEllipse}`} />
             </div>
-            <div className="bg-neutral-900  rounded-lg h-80 md:w-10/12 w-full flex flex-col justify-center items-center">
+            <div className="bg-neutral-700/25 backdrop-blur-lg group-hover:bg-gradient-to-b group-hover:from-[#312E81] group-hover:to-indigo-500 rounded-lg h-80 md:w-10/12 w-full flex flex-col justify-center items-center">
               <img className="mb-4" src={`${Solana}`} />
               <span className="text-xl">Solana</span>
             </div>
