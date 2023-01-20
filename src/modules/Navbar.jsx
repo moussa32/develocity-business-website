dbc8d05e46a557827846cbb09d374402149f5f66
mozes32 <moussaebrahem32@gmail.com>
2023-01-20 02:22:15 +0200

    fix: navbar responsive

--- src/modules/Navbar.jsx
+++ src/modules/Navbar.jsx
@@ -4,11 +4,11 @@ import navLogo from "../assets/NavLogo.svg";
 const Navbar = () => {
   return (
     <nav className="container pt-12 mx-auto relative">
-      <div className="flex flex-col md:flex-row items-center">
+      <div className="flex flex-col lg:flex-row items-center">
         <img src={`${navLogo}`} alt="logo" className="md:block hidden" />
         <div
-          className="w-[300px] md:w-[403px] hidden mt-3 h-[53px] bg-opacity-5 bg-white text-neutral-300 text-base
-        rounded-[20px] md:flex md:mt-0 justify-evenly items-center backdrop-blur-md translate-x-2/4 xl:ml-[271px]"
+          className="w-[300px] lg:w-[403px] hidden mt-3 h-[53px] bg-opacity-5 bg-white text-neutral-300 text-base
+        rounded-[20px] md:flex lg:mt-0 justify-evenly items-center backdrop-blur-md lg:translate-x-2/4 xl:ml-[271px]"
         >
           <a className="transition duration-500 hover:text-indigo-400 hover:bg-white/5 px-4 py-2 rounded-lg cursor-pointer">
             Home
--- src/modules/SideMenu.jsx
+++ src/modules/SideMenu.jsx
@@ -21,7 +21,7 @@ const SideMenu = () => {
         <>
           <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8 ">
             <div className="relative flex h-16 items-center justify-between">
-              <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+              <div className="absolute inset-y-0 left-0 flex items-center lg:hidden">
                 {/* Mobile menu button*/}
                 <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-neutral-300 hover:bg-opacity-5 hover:bg-white hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
                   <span className="sr-only">Open main menu</span>
