af16e4b64be5ed2fba9cd141bd7f687334053e45
mozes32 <moussaebrahem32@gmail.com>
2023-01-21 18:30:56 +0200

    fix: modal over menu

--- src/modules/QuoteModal.jsx
+++ src/modules/QuoteModal.jsx
@@ -9,9 +9,9 @@ const QuoteModal = () => {
   const [open, setOpen] = useState(false);
 
   const cancelButtonRef = useRef(null);
-  const closeModal = () =>{
-    setOpen(false)
-  }
+  const closeModal = () => {
+    setOpen(false);
+  };
   return (
     <>
       <button
@@ -22,12 +22,7 @@ const QuoteModal = () => {
         <img className="inline pl-2" src={`${GoArrowIcon}`} alt="Go arrow" />
       </button>
       <Transition.Root show={open} as={Fragment}>
-        <Dialog
-          as="div"
-          className="relative z-10"
-          initialFocus={cancelButtonRef}
-          onClose={setOpen}
-        >
+        <Dialog as="div" className="relative z-40" initialFocus={cancelButtonRef} onClose={setOpen}>
           <Transition.Child
             as={Fragment}
             enter="ease-out duration-300"
@@ -52,47 +47,25 @@ const QuoteModal = () => {
                 leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
               >
                 <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 w-[800px]">
-                  <h1 className="text-center text-5xl text-[#171717] font-semibold mt-10">
-                    How it works?
-                  </h1>
+                  <h1 className="text-center text-5xl text-[#171717] font-semibold mt-10">How it works?</h1>
                   <div className="flex flex-col md:flex-row justify-around mt-12 text-center">
                     <div>
-                      <img
-                        className="mt-3 md:mt-0 mx-auto"
-                        src={DoubleArrowIcon}
-                        alt="DoubleArrowIcon"
-                      />
-                      <p className="px-16 text-base font-medium text-[#404040] mt-3">
-                        Tell us about your needs
-                      </p>
+                      <img className="mt-3 md:mt-0 mx-auto" src={DoubleArrowIcon} alt="DoubleArrowIcon" />
+                      <p className="px-16 text-base font-medium text-[#404040] mt-3">Tell us about your needs</p>
                     </div>
                     <div>
-                      <img
-                        className="mt-3 md:mt-0 mx-auto"
-                        src={DoubleArrowIcon}
-                        alt="DoubleArrowIcon"
-                      />
-                      <p className="px-16 text-base font-medium text-[#404040] mt-3">
-                        We‘ll discuss the project
-                      </p>
+                      <img className="mt-3 md:mt-0 mx-auto" src={DoubleArrowIcon} alt="DoubleArrowIcon" />
+                      <p className="px-16 text-base font-medium text-[#404040] mt-3">We‘ll discuss the project</p>
                     </div>
                     <div>
-                      <img
-                        className="mt-3 md:mt-0 mx-auto"
-                        src={DoubleArrowIcon}
-                        alt="DoubleArrowIcon"
-                      />
-                      <p className="px-16 text-base font-medium text-[#404040] mt-3">
-                        You‘ll get a free quote from us
-                      </p>
+                      <img className="mt-3 md:mt-0 mx-auto" src={DoubleArrowIcon} alt="DoubleArrowIcon" />
+                      <p className="px-16 text-base font-medium text-[#404040] mt-3">You‘ll get a free quote from us</p>
                     </div>
                   </div>
                   <form className="px-5 mt-16">
                     <div className="grid grid-cols-1 md:grid-cols-2">
                       <div className="mx-auto">
-                        <label className="block text-sm text-[#737373] font-medium ml-2 mb-2">
-                          Name *
-                        </label>
+                        <label className="block text-sm text-[#737373] font-medium ml-2 mb-2">Name *</label>
                         <input
                           className="border border-[#6466E9] rounded h-14 md:w-[312px]"
                           type="text"
@@ -100,9 +73,7 @@ const QuoteModal = () => {
                         />
                       </div>
                       <div className="mx-auto mt-7 md:mt-0">
-                        <label className="block text-sm text-[#737373] font-medium ml-2 mb-2">
-                          Last Name *
-                        </label>
+                        <label className="block text-sm text-[#737373] font-medium ml-2 mb-2">Last Name *</label>
                         <input
                           className="border border-[#6466E9] rounded h-14 md:w-[312px]"
                           type="text"
@@ -110,19 +81,11 @@ const QuoteModal = () => {
                         />
                       </div>
                       <div className="mx-auto mt-7">
-                        <label className="block text-sm text-[#737373] font-medium ml-2 mb-2">
-                          Email *
-                        </label>
-                        <input
-                          className="border border-[#6466E9] rounded h-14 md:w-[312px]"
-                          type="text"
-                          name="email"
-                        />
+                        <label className="block text-sm text-[#737373] font-medium ml-2 mb-2">Email *</label>
+                        <input className="border border-[#6466E9] rounded h-14 md:w-[312px]" type="text" name="email" />
                       </div>
                       <div className="mx-auto mt-7">
-                        <label className="block text-sm text-[#737373] font-medium ml-2 mb-2">
-                          Company *
-                        </label>
+                        <label className="block text-sm text-[#737373] font-medium ml-2 mb-2">Company *</label>
                         <input
                           className="border border-[#6466E9] rounded h-14 md:w-[312px]"
                           type="text"
@@ -131,9 +94,7 @@ const QuoteModal = () => {
                       </div>
                     </div>
                     <div className="mx-auto mt-7 w-[250px] md:w-[695px]">
-                      <label className="block text-sm text-[#737373] font-medium ml-2 mb-2">
-                        About your project *
-                      </label>
+                      <label className="block text-sm text-[#737373] font-medium ml-2 mb-2">About your project *</label>
                       <textarea
                         className="border border-[#6466E9] rounded h-44 w-[250px] md:w-[695px]"
                         name="about-project"
@@ -141,12 +102,10 @@ const QuoteModal = () => {
                     </div>
                     <div className="ml-2 md:ml-8">
                       <input name="NDA-checkbox" type="checkbox" />
-                      <label className="text-sm font-medium text-[#404040] ml-2">
-                        I require NDA
-                      </label>
+                      <label className="text-sm font-medium text-[#404040] ml-2">I require NDA</label>
                     </div>
                   </form>
-                  <CompeleteInquiry handleClick = {() => closeModal()} />
+                  <CompeleteInquiry handleClick={() => closeModal()} />
                 </Dialog.Panel>
               </Transition.Child>
             </div>
