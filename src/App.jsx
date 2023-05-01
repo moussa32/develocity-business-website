f24b370b9cb2aee2b65cfa9a18c44e54acb75206
mozes32 <moussaebrahem32@gmail.com>
2023-05-01 17:02:32 +0300

    refactor: added title & description and keywords

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Home from "./modules/Home";
 import Layout from "./modules/Layout/Layout";
 import PricingPage from "./modules/Pricing/PricingPage";
+import { Helmet } from "react-helmet";
 
 const router = createBrowserRouter([
   {
@@ -20,7 +21,22 @@ const router = createBrowserRouter([
 ]);
 
 const App = () => {
-  return <RouterProvider router={router} />;
+  return (
+    <>
+      <Helmet>
+        <title>Develocity Solutions | Blockchain, NFT, Gaming, Mobile Apps & SaaS Solutions</title>
+        <meta
+          name="descrption"
+          content="Explore Develocity.solutions for cutting-edge blockchain, NFT, NFT gaming, mobile app, and SaaS development services. Elevate your business with our innovative, secure, and user-friendly digital solutions."
+        />
+        <meta
+          name="keywords"
+          content="blockchain development, NFT marketplace, NFT gaming, mobile app development, SaaS development, digital transformation, Develocity Ssolutions, decentralized technology, non-fungible tokens, innovative solutions"
+        />
+      </Helmet>
+      <RouterProvider router={router} />
+    </>
+  );
 };
 
 export default App;
